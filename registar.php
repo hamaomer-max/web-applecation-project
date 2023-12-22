57e07744b3a95502c40df8460ce7d5ce83d8762b
hama <hama@gmail.com>
2023-12-22 16:38:00 +0300

    deletin some file

--- registar.php
+++ registar.php
@@ -1,24 +1,24 @@
-<?php require_once "nav.php";in(0); ?> 
+<?php require_once "nav2.php";in(1); ?> 
 
 <?php
 if (isset($_POST['submit'])) {
     $user->username = $_POST['username'];
     $user->email = $_POST['email'];
     $user->password = hash('gost',$_POST['password']);
-    $user->rule = $_POST['rule'];
+    $user->rule = 1;
     
     if ($user->create() === true) {
-        header("location:index.php");
+        header("location:registar_user.php");
     }else {
         echo "not add";
     }
 }
 ?>
-<form action="registar.php" class="col-lg-4 col-sm text-center shadow-lg p-3 rounded-5 m-auto w-50 mt-4" style="background:linear-gradient(purple,white);" method="post" enctype="multipart/form-data">
+<form action="registar.php" class="col-lg-4 col-sm text-center shadow-lg p-3 rounded-5 m-auto w-50 mt-4" style="background:linear-gradient(purple,white);" method="post">
 <img src="./photo/car.png" width="100">
-<input type="text" name="username" placeholder="username" class="form-control m-1 border-0 shadow-none">
-<input type="text" name="email" placeholder="email" class="form-control m-1 border-0 shadow-none">
-<input type="text" name="password" placeholder="password" class="form-control m-1 border-0 shadow-none">
+<input type="text" name="username" placeholder="username" class="form-control m-1 border-0 shadow-none" required>
+<input type="text" name="email" placeholder="email" class="form-control m-1 border-0 shadow-none" required>
+<input type="text" name="password" placeholder="password" class="form-control m-1 border-0 shadow-none" required>
 <button name="submit" class="btn btn-primary rounded-4 fw-bold mt-4 w-40 border-0" style="background:linear-gradient(purple,black) ;">Registar</button>
 <a href="ui.php" class="btn btn-primary rounded-4 fw-bold mt-4 w-40 border-0">Back</a>
 </form>
